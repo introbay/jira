ea825af87790e2e78ce9518e92226ba0f8541e54
Ignacio L Flores <ignacio@introbay.com>
2015-12-14 00:17:15 +0100

    Not necessary to apt-get update

--- install_cmds.sh
+++ install_cmds.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Install Atlassian Jira
-apt-get -y update
-
-## Now Install Atlassian Jira
+# Now Install Atlassian Jira
 sh /opt/atlassian-$AppName-$AppVer-jira-$AppVer-$Arch.bin -q -varfile /opt/response.varfile
 
 # Clean up
