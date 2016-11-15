fa5cc70ce60caff3c2e88c957bf3277ae3227726
Ignacio López Flores <ignacio@introbay.com>
2016-11-15 18:58:54 +0100

    Corrected bug adapting to jira-core

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t introbay/jira:7.1.6 .
--- install_cmds.sh
+++ install_cmds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install Atlassian Jira
 # Now Install Atlassian Jira
-sh /opt/atlassian-$AppName-$AppVer-jira-$AppVer-$Arch.bin -q -varfile /opt/response.varfile
+sh /opt/atlassian-$AppName-$AppVer-$Arch.bin -q -varfile /opt/response.varfile
 
 # Clean up
-rm -f /opt/atlassian-$AppName-$AppVer-jira-$AppVer-$Arch.bin
+rm -f /opt/atlassian-$AppName-$AppVer-$Arch.bin
